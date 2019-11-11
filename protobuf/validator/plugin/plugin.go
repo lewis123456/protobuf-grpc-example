58b18ba287758c0c2c3e9dc036e5a673cdd59f97
lewis123456 <2328811532@qq.com>
2019-11-11 23:58:04 +0800

    add

--- protobuf/validator/plugin/plugin.go
+++ protobuf/validator/plugin/plugin.go
@@ -1,6 +1,7 @@
 package plugin
 
 import (
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/golang/protobuf/protoc-gen-go/generator"
 )
 
@@ -20,14 +21,45 @@ func (v *validator) Init(g *generator.Generator) {
 	v.gen = g
 }
 func (v *validator) Generate(file *generator.FileDescriptor) {
-
+	v.gen.Reset()
+	msgs := getValidatorMsg(file.MessageType)
+	for _, msg := range file.MessageType {
+		if file.GetSyntax() == "proto3" {
+			v.generateValidator(file, msg, "", msgs)
+		}
+	}
 }
 func (v *validator) GenerateImports(file *generator.FileDescriptor) {}
 
-func GetValidatorMsg(desc []*generator.Descriptor) []string {
-
+func getValidatorMsg(desc []*descriptor.DescriptorProto) []string {
+	var msgs []string
+	for _, msg := range desc {
+		if hasValidatorOption(msg) {
+			msgs = append(msgs, *msg.Name)
+		}
+		if len(msg.NestedType) != 0 {
+			msgs = append(msgs, getValidatorMsg(msg.NestedType)...)
+		}
+	}
+	return msgs
 }
 
-func HasValidatorOption(msg *generator.Descriptor) bool {
+func hasValidatorOption(msg *descriptor.DescriptorProto) bool {
+	for _, field := range msg.Field {
+		if field.Options != nil {
+			return true
+		}
+	}
+	return false
+}
+
+func (v *validator) P(str ...interface{}) {
+	v.gen.P(str...)
+}
+
+func (v *validator) generateValidator(file *generator.FileDescriptor, msg *descriptor.DescriptorProto, prefix string, msgs []string) {
+	if !hasValidatorOption(msg) {
+		return
+	}
 
 }
